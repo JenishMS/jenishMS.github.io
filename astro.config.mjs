6f718ad447e8e3f84d69a07a83341e610b1996a2
Jenish MS <jenishmstephen@gmail.com>
2023-08-02 07:45:46 +0530

    revert output type config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jenishms.github.io",
-  // base: "/",
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
   integrations: [tailwind(), react()],
 });
