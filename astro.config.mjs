e1f219efe85d709c8c3b11d9c3f33cce0d7e5dc3
Jenish MS <jenish@theproindia.com>
2025-07-21 16:02:06 +0530

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: "https://jenishms.in",
+  site: "https://www.jenishms.in",
   build: {
     outDir: "dist",
   },
